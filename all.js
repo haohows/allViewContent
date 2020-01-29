a3c3a560219743ca080863ef19b866931e51e2e6
faraway520 <49857978+faraway520@users.noreply.github.com>
2020-01-29 23:28:19 +0800

    橫屏模式提醒語

--- all.js
+++ all.js
@@ -14,3 +14,50 @@ function allView() {
         elem.webkitRequestFullscreen();
     }
 }
+
+
+let upright = document.querySelector('.upright')
+let horizontal = document.querySelector('.horizontal')
+
+// 判斷手機是否為橫版瀏覽
+function hengshuping() {
+
+    if (window.orientation == 180 || window.orientation == 0) {
+        upright.classList.remove('hide')
+        horizontal.classList.add('hide')
+
+        console.log('直立')
+    }
+    if (window.orientation == 90 || window.orientation == -90) {
+        horizontal.classList.remove('hide')
+        upright.classList.add('hide')
+        console.log('水平')
+    }
+
+}
+window.addEventListener("onorientationchange" in window ? "orientationchange" : "resize", hengshuping, false);
+
+
+if (window.orientation == 180 || window.orientation == 0) {
+    upright.classList.remove('hide')
+    horizontal.classList.add('hide')
+
+    console.log('直立')
+}
+if (window.orientation == 90 || window.orientation == -90) {
+    horizontal.classList.remove('hide')
+    upright.classList.add('hide')
+    console.log('水平')
+}
+
+
+
+// function isMobile() {
+//     try { document.createEvent("TouchEvent"); return true; } catch (e) { return false; }
+// }
+
+// if (isMobile()) {
+//     console.log('載具')
+// } else {
+//     console.log('PC')
+// }
